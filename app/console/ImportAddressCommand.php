cd08053d80c7acaf39280568e56cb1f2915791c5
Pavel Ryznar <pryznar@gmail.com>
2015-12-16 10:18:48 +0100

    console: added ImportAddressCommand

---
+++ app/console/ImportAddressCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Console;
+
+use StreetApi\Services\ImportAddressService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class ImportAddressCommand extends Command
+{
+
+	protected function configure()
+	{
+		$this->setName('import:address')->setDescription('Import new addresses');
+	}
+
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		/** @var ImportAddressService $importAddressService */
+		$importAddressService = $this->getHelper('container')->getByType('StreetApi\Services\ImportAddressService');
+
+		$xmlFile = simplexml_load_file($importAddressService->getRootDir() . '/../adresy.xml');
+
+		try {
+			$output->writeLn('<info>Start importing addresses</info>');
+			$totalCount = $xmlFile->count();
+			$output->writeln(PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL);
+			$progressBar = new ProgressBar($output, $totalCount);
+			$progressBar->setFormat('%message%' . PHP_EOL . '%bar% %percent:3s% %' . PHP_EOL . 'count: %current%/%max%' . PHP_EOL . 'time:  %elapsed:6s%/%estimated:-6s%' . PHP_EOL);
+			$progressBar->setBarCharacter('<info>■</info>');
+			$progressBar->setEmptyBarCharacter(' ');
+			$progressBar->setProgressCharacter('');
+			$progressBar->setRedrawFrequency(ceil(($totalCount / 100)));
+			$progressBar->start();
+
+			$importAddressService->import($xmlFile, $progressBar);
+
+			$output->writeLn(PHP_EOL . '<info>Importing addresses finished</info>');
+			return 0;
+		} catch (\Exception $e) {
+			$output->writeLn('<error>' . $e->getMessage() . '</error>');
+			return 1;
+		}
+
+	}
+
+}
